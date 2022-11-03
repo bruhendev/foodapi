8d29fee87f7ffdb9a8dae0fc85b8e289477bd257
Bruno Henrique <brunohenrique242@gmail.com>
2022-11-03 16:21:59 -0300

    iniciando metodo buscar estado por id

--- src/main/java/com/spring/foodapi/api/controller/EstadoController.java
+++ src/main/java/com/spring/foodapi/api/controller/EstadoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class EstadoController {
 
     @GetMapping
     public ResponseEntity<List<Estado>> listar() {
+        return ResponseEntity.ok(cadastroEstado.listar());
+    }
+
+    @GetMapping("/{estadoId}")
+    public ResponseEntity<List<Estado>> buscar(@PathVariable Long estadoId) {
         return ResponseEntity.ok(cadastroEstado.listar());
     }
 }
