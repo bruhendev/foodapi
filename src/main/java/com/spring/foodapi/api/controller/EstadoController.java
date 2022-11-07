0e222b91b309ad6a88293e813619810b32e2fc98
Bruno <brunohenrique242@gmail.com>
2022-11-07 08:02:15 -0300

    Iniciando com o metodo patch

--- src/main/java/com/spring/foodapi/api/controller/EstadoController.java
+++ src/main/java/com/spring/foodapi/api/controller/EstadoController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.foodapi.domain.exception.EntidadeNaoEncontradaException;
--- src/main/java/com/spring/foodapi/api/controller/RestauranteController.java
+++ src/main/java/com/spring/foodapi/api/controller/RestauranteController.java
@@ -9,12 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.foodapi.domain.exception.EntidadeNaoEncontradaException;
@@ -24,7 +24,7 @@ import com.spring.foodapi.domain.service.CadastroRestauranteService;
 @RestController
 @RequestMapping("/restaurantes")
 public class RestauranteController {
-    
+
     @Autowired
     private CadastroRestauranteService cadastroRestaurante;
 
@@ -37,13 +37,12 @@ public class RestauranteController {
     public ResponseEntity<Restaurante> buscar(@PathVariable("restauranteId") Long restauranteId) {
         try {
             return ResponseEntity.ok(cadastroRestaurante.buscar(restauranteId));
-        } catch(EntidadeNaoEncontradaException e) {
+        } catch (EntidadeNaoEncontradaException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
         try {
             restaurante = cadastroRestaurante.salvar(restaurante);
@@ -56,14 +55,15 @@ public class RestauranteController {
     }
 
     @PutMapping("/{restauranteId}")
-    public ResponseEntity<?> altualizar(@RequestBody Restaurante restaurante, @PathVariable("restauranteId") Long restauranteId) {
+    public ResponseEntity<?> atualizar(@RequestBody Restaurante restaurante,
+            @PathVariable("restauranteId") Long restauranteId) {
         try {
             restaurante = cadastroRestaurante.atualizar(restaurante, restauranteId);
             return ResponseEntity.status(HttpStatus.OK).body(restaurante);
         } catch (EntidadeNaoEncontradaException e) {
-           Map<String, String> error = new HashMap<>();
-           error.put("error", e.getMessage());
-           return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+            Map<String, String> error = new HashMap<>();
+            error.put("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
         }
     }
 
@@ -74,9 +74,26 @@ public class RestauranteController {
             return ResponseEntity.noContent().build();
         } catch (EntidadeNaoEncontradaException e) {
             Map<String, String> error = new HashMap<>();
-           error.put("error", e.getMessage());
-           return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+            error.put("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
         }
     }
-    
+
+    @PatchMapping("/{restauranteId}")
+    public ResponseEntity<?> atualizarParcial(@PathVariable("restauranteId") Long restaurnateId,
+            @RequestBody Map<String, Object> campos) {
+        
+        Restaurante restauranteAtual = cadastroRestaurante.buscar(restaurnateId);
+
+        merge(campos, restauranteAtual);
+
+        return atualizar(restauranteAtual, restaurnateId)
+    }
+
+    private void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino){
+        camposOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+            System.out.println(nomePropriedade + " = " + valorPropriedade);
+        });
+    }
+
 }
