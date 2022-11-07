4c373639d08fbebcd1785e16e7f6c8d2bd00ac2a
Bruno Henrique <brunohenrique242@gmail.com>
2022-11-07 16:18:40 -0300

    Iniciando metodo buscarPorNome

--- src/main/java/com/spring/foodapi/api/controller/CozinhaController.java
+++ src/main/java/com/spring/foodapi/api/controller/CozinhaController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -55,6 +56,11 @@ public class CozinhaController {
         // headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
 
         // return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+    }
+
+    @GetMapping("/por-nome")
+    public ResponseEntity<?> buscarPorNome(@RequestParam(name = "nome") String nome ) {
+        return null;
     }
 
     @PostMapping
